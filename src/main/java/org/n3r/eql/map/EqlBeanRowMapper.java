f2244d0b5a9a24f6cd9e41f87cffe84d4670b568
bingoohuang <bingoo.huang@gmail.com>
2014-08-13 13:22:49 +0800

    try set bean's property by setter first

--- src/main/java/org/n3r/eql/map/EqlBeanRowMapper.java
+++ src/main/java/org/n3r/eql/map/EqlBeanRowMapper.java
@@ -4,6 +4,7 @@ import org.n3r.eql.joor.Reflect;
 import org.n3r.eql.util.EqlUtils;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -22,13 +23,27 @@ public class EqlBeanRowMapper extends EqlBaseBeanMapper implements EqlRowMapper 
         for (int index = 1; index <= columnCount; index++) {
             String column = EqlUtils.lookupColumnName(rsmd, index);
             PropertyDescriptor pd = this.mappedFields.get(column.replaceAll(" ", "").toLowerCase());
-            if (pd != null) {
-                Object value = EqlUtils.getResultSetValue(rs, index, pd.getPropertyType());
-                Reflect.on(mappedObject).set(pd.getName(), value);
-            }
+            if (pd == null) continue;
+
+            Object value = EqlUtils.getResultSetValue(rs, index, pd.getPropertyType());
+            setBeanProperty(mappedObject, pd, value);
         }
 
         return mappedObject;
+    }
+
+    private void setBeanProperty(Object object, PropertyDescriptor pd, Object value) {
+        Method setter = pd.getWriteMethod();
+        if (setter != null) {
+            try {
+                setter.invoke(object, value);
+                return;
+            } catch (Exception e) {
+
+            }
+        }
+
+        Reflect.on(object).set(pd.getName(), value);
     }
 
 }
