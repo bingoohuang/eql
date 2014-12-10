3d6eac7a2730fd4d1a051e41d435940c000d1eae
bingoohuang <bingoo.huang@gmail.com>
2014-12-10 09:54:33 +0800

    isEmpty support collection, map , iterable and charsequence

--- src/main/java/org/n3r/eql/parser/IsEmptyPart.java
+++ src/main/java/org/n3r/eql/parser/IsEmptyPart.java
@@ -3,6 +3,9 @@ package org.n3r.eql.parser;
 import org.n3r.eql.base.ExpressionEvaluator;
 import org.n3r.eql.map.EqlRun;
 
+import java.util.Collection;
+import java.util.Map;
+
 public class IsEmptyPart implements EqlPart {
     protected final String expr;
     protected final MultiPart multiPart;
@@ -22,13 +25,25 @@ public class IsEmptyPart implements EqlPart {
     protected boolean isEmpty(EqlRun eqlRun) {
         ExpressionEvaluator evaluator = eqlRun.getEqlConfig().getExpressionEvaluator();
         Object target = evaluator.eval(expr, eqlRun);
-        return target == null || target.toString().length() == 0;
+
+        return isEmpty(target) ? true : target.toString().length() == 0;
     }
 
     protected boolean isBlank(EqlRun eqlRun) {
         ExpressionEvaluator evaluator = eqlRun.getEqlConfig().getExpressionEvaluator();
         Object target = evaluator.eval(expr, eqlRun);
-        return target == null || target.toString().trim().length() == 0;
+
+        return isEmpty(target) ? true : target.toString().trim().length() == 0;
+    }
+
+    public static boolean isEmpty(Object target) {
+        if (target == null) return true;
+        if (target instanceof Collection) return ((Collection) target).isEmpty();
+        if (target instanceof Map) return ((Map) target).isEmpty();
+        if (target instanceof Iterable) return !((Iterable) target).iterator().hasNext();
+        if (target instanceof CharSequence) return ((CharSequence)target).length() == 0;
+
+        return false;
     }
 
     protected boolean isNull(EqlRun eqlRun) {
@@ -36,5 +51,4 @@ public class IsEmptyPart implements EqlPart {
         Object target = evaluator.eval(expr, eqlRun);
         return target == null;
     }
-
 }
