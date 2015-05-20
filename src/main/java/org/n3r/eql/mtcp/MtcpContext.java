da3f105dbf678e56efcae29cb5f0e812b42bff0f
fengyu <fengyu@raiyee.com>
2015-05-20 10:23:02 +0800

    add tcode

--- src/main/java/org/n3r/eql/mtcp/MtcpContext.java
+++ src/main/java/org/n3r/eql/mtcp/MtcpContext.java
@@ -4,18 +4,29 @@ import org.slf4j.MDC;
 
 public class MtcpContext {
     private static ThreadLocal<String> tenantIdLocal = new InheritableThreadLocal<String>();
+    private static ThreadLocal<String> tenantCodeLocal = new InheritableThreadLocal<String>();
 
     public static void setTenantId(String tenantId) {
         tenantIdLocal.set(tenantId);
         MDC.put("tenantId", tenantId);
     }
 
+    public static void setTenantCode(String tenantCode) {
+        tenantCodeLocal.set(tenantCode);
+        MDC.put("tenantCode", tenantCode);
+    }
+
     public static String getTenantId() {
         return tenantIdLocal.get();
     }
+    public static String getTenantCode() {
+        return tenantCodeLocal.get();
+    }
 
-    public static void clearTenantId() {
+    public static void clear() {
         MDC.remove(tenantIdLocal.get());
+        MDC.remove(tenantCodeLocal.get());
         tenantIdLocal.remove();
+        tenantCodeLocal.remove();
     }
 }
