4d8c170685920e07a4b02f959e20886b88d8baa6
bingoohuang <bingoohuang@bingoohuangdeMacBook-Pro.local>
2015-07-31 15:34:44 +0800

    add GetConnectionTest

---
+++ src/test/java/org/n3r/eql/impl/GetConnectionTest.java
@@ -0,0 +1,35 @@
+package org.n3r.eql.impl;
+
+import org.junit.Test;
+import org.n3r.eql.Eql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class GetConnectionTest {
+    @Test
+    public void getJdbcConnection() throws SQLException {
+        Connection connection = new Eql("me").getConnection();
+        assertThat(connection, is(notNullValue()));
+        connection.close();
+
+        connection = new Eql("me").getConnection();
+        assertThat(connection, is(notNullValue()));
+        connection.close();
+    }
+
+    @Test
+    public void getJndiConnection() throws SQLException {
+        Connection connection = new Eql("jndi").getConnection();
+        assertThat(connection, is(notNullValue()));
+        connection.close();
+
+        connection = new Eql("jndi").getConnection();
+        assertThat(connection, is(notNullValue()));
+        connection.close();
+    }
+}
