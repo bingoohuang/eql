03b1a9ba444254279e4653ce0d82f4ef6959a3d0
AL_WUHC <wuhc2@asiainfo-linkage.com>
2015-10-14 14:46:14 +0800

    ognl evaluate root add util methods.

---
+++ src/main/java/org/n3r/eql/util/OgRoot.java
@@ -0,0 +1,49 @@
+package org.n3r.eql.util;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OgRoot extends HashMap<String, Object> {
+
+    private static final long serialVersionUID = -1;
+
+    public OgRoot() {
+        super();
+    }
+
+    public OgRoot(Map<? extends String, ? extends Object> m) {
+        super(m);
+    }
+
+    public boolean isNull(Object target) {
+        return target == null;
+    }
+
+    public boolean isNotNull(Object target) {
+        return target != null;
+    }
+
+    public boolean isEmpty(Object target) {
+        if (target == null) return true;
+        if (target instanceof Collection) return ((Collection) target).isEmpty();
+        if (target instanceof Map) return ((Map) target).isEmpty();
+        if (target instanceof Iterable) return !((Iterable) target).iterator().hasNext();
+        if (target instanceof CharSequence) return ((CharSequence)target).length() == 0;
+        if (target.getClass().isArray()) return ((Object[])target).length == 0;
+
+        return false;
+    }
+
+    public boolean isNotEmpty(Object target) {
+        return !isEmpty(target);
+    }
+
+    public boolean isBlank(Object target) {
+        return isEmpty(target) || target.toString().trim().length() == 0;
+    }
+
+    public boolean isNotBlank(Object target) {
+        return !isBlank(target);
+    }
+}
