ccd6404beeff6bc1ce88ba0a0005e04f7c55ac9d
bingoo <bingoo.huang@gmail.com>
2018-08-23 15:19:35 +0800

    refactor test case for joda type mapping

--- src/test/java/org/n3r/eql/datetime/SingleDateTimeTest.java
+++ src/test/java/org/n3r/eql/datetime/SingleDateTimeTest.java
@@ -6,7 +6,10 @@ import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.n3r.eql.Eql;
 import org.n3r.eql.eqler.EqlerFactory;
 import org.n3r.eql.eqler.annotations.EqlerConfig;
 import org.n3r.eql.eqler.annotations.Sql;
@@ -14,6 +17,15 @@ import org.n3r.eql.eqler.annotations.Sql;
 import static com.google.common.truth.Truth.assertThat;
 
 public class SingleDateTimeTest {
+    @BeforeClass
+    public static void beforeClass() {
+        new Eql("h2").execute(
+                "DROP TABLE IF EXISTS t_localdate; " +
+                        "CREATE TABLE t_localdate (id int, local_date date);" +
+                        "insert into t_localdate values(100, '2017-10-20') ");
+    }
+
+
     @EqlerConfig("h2")
     public interface DateTimeDao {
         @Sql("SELECT ##")
@@ -26,78 +38,96 @@ public class SingleDateTimeTest {
         LocalTime queryLocalTime(LocalTime dateTime);
     }
 
+    static DateTimeDao dao = EqlerFactory.getEqler(DateTimeDao.class);
+
+    @Test
+    public void jodaDateTime() {
+        DateTime dateTime = parseDateAsDateTime("2017-10-20");
+        val dt = dao.queryDateTime(dateTime);
+        assertThat(dt).isEqualTo(dateTime);
+    }
+
+    @Test
+    public void jodaLocalDate() {
+        LocalDate dateTime = parseDateAsLocalDate("2017-10-20");
+        val ld = dao.queryLocalDate(dateTime);
+        assertThat(ld).isEqualTo(dateTime);
+    }
+
+    @Test
+    public void jodaLocalTime() {
+        LocalTime dateTime = parseTimeAsLocalTime("14:14:14");
+        val lt = dao.queryLocalTime(dateTime);
+        assertThat(lt).isEqualTo(dateTime);
+    }
+
     @Data
     public static class JodaBean {
         private DateTime dt;
-        private LocalDate ld;
-        private LocalTime lt;
-    }
+        private LocalDate localDate;
+        private LocalTime localTime;
 
-    @Test
-    public void test1() {
-        val dao = EqlerFactory.getEqler(DateTimeDao.class);
-        val dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-        val dateTime = dateTimeFormatter.parseDateTime("2017-10-20");
-        val quered = dao.queryDateTime(dateTime);
-        assertThat(quered.toString(dateTimeFormatter)).isEqualTo("2017-10-20");
-    }
+        public JodaBean(DateTime dt) {
+            this.dt = dt;
+        }
 
-    @Test
-    public void jodaLocalDateTest() {
-        val dao = EqlerFactory.getEqler(DateTimeDao.class);
-        val dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-        val dateTime = dateTimeFormatter.parseLocalDate("2017-10-20");
-        val quered = dao.queryLocalDate(dateTime);
-        assertThat(quered.toString(dateTimeFormatter)).isEqualTo("2017-10-20");
-    }
+        public JodaBean(LocalDate localDate) {
+            this.localDate = localDate;
+        }
 
-    @Test
-    public void jodaLocalTimeTest() {
-        val dao = EqlerFactory.getEqler(DateTimeDao.class);
-        val dateTimeFormatter = DateTimeFormat.forPattern("HH:mm:ss");
-        val dateTime = dateTimeFormatter.parseLocalTime("14:14:14");
-        val quered = dao.queryLocalTime(dateTime);
-        assertThat(quered.toString(dateTimeFormatter)).isEqualTo("14:14:14");
+        public JodaBean(LocalTime localTime) {
+            this.localTime = localTime;
+        }
     }
-
 
     @EqlerConfig("h2")
     public interface DateTimeDao2 {
-        @Sql("SELECT ## as dt")
-        JodaBean queryDateTime(DateTime dateTime);
+        @Sql("SELECT #dt# as dt")
+        JodaBean queryDateTime(JodaBean bean);
 
-        @Sql("SELECT ## as ld")
-        JodaBean queryLocalDate(LocalDate dateTime);
+        @Sql("SELECT id, local_date from t_localdate where #localDate# is not null")
+        JodaBean queryLocalDate(JodaBean bean);
 
-        @Sql("SELECT ## as lt")
-        JodaBean queryLocalTime(LocalTime dateTime);
+        @Sql("SELECT #localTime# as localTime")
+        JodaBean queryLocalTime(JodaBean bean);
     }
 
 
+    static DateTimeDao2 dao2 = EqlerFactory.getEqler(DateTimeDao2.class);
+
     @Test
     public void test2() {
-        val dao = EqlerFactory.getEqler(DateTimeDao2.class);
-        val dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-        val dateTime = dateTimeFormatter.parseDateTime("2017-10-20");
-        val quered = dao.queryDateTime(dateTime);
-        assertThat(quered.getDt().toString(dateTimeFormatter)).isEqualTo("2017-10-20");
+        DateTime dt1 = parseDateAsDateTime("2017-10-20");
+        val dt = dao2.queryDateTime(new JodaBean(dt1));
+        assertThat(dt.getDt()).isEqualTo(dt1);
     }
 
     @Test
     public void jodaLocalDateTest2() {
-        val dao = EqlerFactory.getEqler(DateTimeDao2.class);
-        val dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-        val dateTime = dateTimeFormatter.parseLocalDate("2017-10-20");
-        val quered = dao.queryLocalDate(dateTime);
-        assertThat(quered.getLd().toString(dateTimeFormatter)).isEqualTo("2017-10-20");
+        LocalDate localDate = parseDateAsLocalDate("2017-10-20");
+        val ld = dao2.queryLocalDate(new JodaBean(localDate));
+        assertThat(ld.getLocalDate()).isEqualTo(localDate);
     }
 
     @Test
     public void jodaLocalTimeTest2() {
-        val dao = EqlerFactory.getEqler(DateTimeDao2.class);
-        val dateTimeFormatter = DateTimeFormat.forPattern("HH:mm:ss");
-        val dateTime = dateTimeFormatter.parseLocalTime("14:14:14");
-        val quered = dao.queryLocalTime(dateTime);
-        assertThat(quered.getLt().toString(dateTimeFormatter)).isEqualTo("14:14:14");
+        LocalTime localTime = parseTimeAsLocalTime("14:14:14");
+        val lt = dao2.queryLocalTime(new JodaBean(localTime));
+        assertThat(lt.getLocalTime()).isEqualTo(localTime);
+    }
+
+    static DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+    static DateTimeFormatter timeFormatter = DateTimeFormat.forPattern("HH:mm:ss");
+
+    public static LocalTime parseTimeAsLocalTime(String text) {
+        return timeFormatter.parseLocalTime(text);
+    }
+
+    public static DateTime parseDateAsDateTime(String text) {
+        return dateFormatter.parseDateTime(text);
+    }
+
+    public static LocalDate parseDateAsLocalDate(String text) {
+        return dateFormatter.parseLocalDate(text);
     }
 }
