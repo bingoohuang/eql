cff4710246a9526b4a8ff861ebd339895b71968f
bingoohuang <bingoo.huang@gmail.com>
2020-04-09 11:25:55 +0800

    Update EUpdateStmtTest.java

--- src/test/java/org/n3r/eql/EUpdateStmtTest.java
+++ src/test/java/org/n3r/eql/EUpdateStmtTest.java
@@ -6,10 +6,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class EUpdateStmtTest {
+    @Before
+    public void before() {
+        new Eql().id("dropTestTable").execute();
+        new Eql().id("createTestTable").params(new Timestamp(1383122146000l)).execute();
+    }
+
     @Test
     public void test() {
-        new Eql().id("createTestTable").execute();
-
         Eql eql = new Eql().id("updateStmt");
         EUpdateStmt updateStmt = eql.updateStmt();
 
