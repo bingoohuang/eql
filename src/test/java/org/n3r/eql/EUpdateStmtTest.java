6d8b73e2c6ae4bb34eca4088f1d19c7488afcc79
bingoohuang <bingoo.huang@gmail.com>
2020-04-09 11:26:20 +0800

    Update EUpdateStmtTest.java

--- src/test/java/org/n3r/eql/EUpdateStmtTest.java
+++ src/test/java/org/n3r/eql/EUpdateStmtTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertThat;
 public class EUpdateStmtTest {
     @Before
     public void before() {
-        new Eql().id("dropTestTable").execute();
         new Eql().id("createTestTable").params(new Timestamp(1383122146000l)).execute();
     }
 
