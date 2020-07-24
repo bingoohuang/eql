0dc73d7520ebf6a48ebb752c04fb6e8c29b131d9
CharLemAznable <545541819@qq.com>
2020-07-24 14:37:59 +0800

    Fixed _lastResult convert exception.

--- src/test/java/org/n3r/eql/InnerVariablsTest.java
+++ src/test/java/org/n3r/eql/InnerVariablsTest.java
@@ -1,9 +1,12 @@
 package org.n3r.eql;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -29,5 +32,26 @@ public class InnerVariablsTest {
                 .params(new Timestamp(1383122146000l))
                 .execute();
         assertThat(result, is(1));
+
+        EqlTest result2 = new Eql().id("testLastResult2")
+                .params(new Timestamp(1383122146000l))
+                .returnType(EqlTest.class).limit(1)
+                .execute();
+        assertThat(result2.getA(), is(4123));
+        assertThat(result2.getB(), is("D"));
+        assertThat(result2.getC(), is("ABC"));
+        assertThat(result2.getD(), is(new Date(1383122146000l)));
+        assertThat(result2.getE(), is(104));
+    }
+
+    @Getter
+    @Setter
+    static class EqlTest {
+
+        private int a;
+        private String b;
+        private String c;
+        private Date d;
+        private int e;
     }
 }
