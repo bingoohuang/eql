ae43e843d9b15762907bdc1b8a7c3bda1ea3a302
CharLemAznable <545541819@qq.com>
2022-03-01 11:03:43 +0800

    Fixed annotation search test case.

--- src/test/java/org/n3r/eql/eqler/AnnotationTest.java
+++ src/test/java/org/n3r/eql/eqler/AnnotationTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import org.n3r.eql.diamond.Dql;
 import org.n3r.eql.eqler.annotations.EqlerConfig;
 import org.n3r.eql.eqler.generators.Generatable;
+import org.n3r.eql.util.C;
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.core.annotation.AnnotationUtils;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -16,6 +18,7 @@ import java.lang.annotation.Target;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mockStatic;
 
 public class AnnotationTest {
 
@@ -29,6 +32,22 @@ public class AnnotationTest {
         assertNotNull(eqlerConfig2);
         assertEquals(Dql.class, eqlerConfig2.eql());
         assertEquals("TEST", eqlerConfig2.value());
+    }
+
+    @Test
+    public void testAnnotationWithoutSpringCore() {
+        try (val mockC = mockStatic(C.class)) {
+            mockC.when(() -> C.classExists(AnnotationUtils.class.getName())).thenReturn(false);
+
+            val eqlerConfig1 = Generatable.parseEqlerConfig(Test1.class);
+            assertNotNull(eqlerConfig1);
+            assertEquals(Dql.class, eqlerConfig1.eql());
+            assertEquals("DEFAULT", eqlerConfig1.value());
+            val eqlerConfig2 = Generatable.parseEqlerConfig(Test2.class);
+            assertNotNull(eqlerConfig2);
+            assertEquals(Dql.class, eqlerConfig2.eql());
+            assertEquals("DEFAULT", eqlerConfig2.value());
+        }
     }
 
     @Documented
